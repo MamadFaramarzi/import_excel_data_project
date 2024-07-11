a10a51bc5e54538e15de0cc5454e1f5af3c6ee70
MamadFaramarzi <mohammadfaramarzi.f78@gmail.com>
2024-07-11 12:19:00 +0330

    'view some change'

--- my_data/views.py
+++ my_data/views.py
@@ -11,12 +11,14 @@ from .serializers import DataSerializers
 
 @api_view()
 def show_data(request,):
+
+    excel_file = request.FILES.get('Excel_Data.xlsx')
+    df = pd.read_excel(excel_file)
+
     # my_data = Data.objects.all()
     # serializer = DataSerializers(my_data, many=True)
     serializer = DataSerializers(data=df.to_dict(orient='records'), many=True)
 
-    excel_file = request.FILES.get('Excel_Data')
-    df = pd.read_excel(excel_file)
 
     serializer.is_valid(raise_exception=True)
     serializer.validated_data
