b8f6af7e17f3daf03be3be098e47c469e2da9923
MamadFaramarzi <mohammadfaramarzi.f78@gmail.com>
2024-07-11 12:47:30 +0330

    "view some change"

--- my_data/views.py
+++ my_data/views.py
@@ -8,19 +8,77 @@ import pandas as pd
 from .models import Data
 from .serializers import DataSerializers
 
+@api_view(['POST'])
+def show_data(request):
+    if request.method == 'POST' and request.FILES.get('file'):
+        excel_file = request.FILES['file']
+        df = pd.read_excel('Excel_Data.xlsx')
 
-@api_view()
-def show_data(request,):
-
-    excel_file = request.FILES.get('Excel_Data.xlsx')
-    df = pd.read_excel(excel_file)
-
-    # my_data = Data.objects.all()
-    # serializer = DataSerializers(my_data, many=True)
-    serializer = DataSerializers(data=df.to_dict(orient='records'), many=True)
+        serializer = DataSerializers(data=df.to_dict(orient='records'), many=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response({"error": "No file found in request"}, status=status.HTTP_400_BAD_REQUEST)
 
 
-    serializer.is_valid(raise_exception=True)
-    serializer.validated_data
 
-    return Response(serializer.data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from django.shortcuts import render
+# from rest_framework.response import Response
+# from rest_framework.decorators import api_view
+# from rest_framework import status
+
+# import pandas as pd
+
+# from .models import Data
+# from .serializers import DataSerializers
+
+
+# @api_view()
+# def show_data(request,):
+
+#     excel_file = request.FILES.get('Excel_Data.xlsx')
+#     df = pd.read_excel(excel_file)
+
+#     # my_data = Data.objects.all()
+#     # serializer = DataSerializers(my_data, many=True)
+#     serializer = DataSerializers(data=df.to_dict(orient='records'), many=True)
+
+
+#     serializer.is_valid(raise_exception=True)
+#     serializer.validated_data
+
+#     return Response(serializer.data)
+
+
+
